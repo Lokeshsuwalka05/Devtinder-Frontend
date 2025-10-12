9339aae3e9f41c1d4dff457b095cf6ba4a05c7d0
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-12 23:31:25 +0530

    Modified feedSlice

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "../utills/constants";
 import axios from "axios";
 import UserCard from "./UserCard";
@@ -6,27 +6,50 @@ import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../features/feed/feedSlice";
 
 const Feed = () => {
+  const [pageNo, setPageNo] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const feed = useSelector((store) => store.feed);
+  console.log(feed);
   const dispatch = useDispatch();
+  const limit = 5;
   const getFeed = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/feed", {
-        withCredentials: true,
-      });
-      dispatch(addFeed(res?.data?.feedOfUser));
+      const res = await axios.get(
+        `${BASE_URL}/feed?page=${pageNo}&limit=${limit}`,
+        {
+          withCredentials: true,
+        }
+      );
+      const users = res?.data?.feedOfUser || [];
+      if (users.length === 0) {
+        setHasMore(false);
+        return;
+      }
+      dispatch(addFeed(users));
     } catch (e) {
       console.error(e);
     }
   };
   useEffect(() => {
     getFeed();
-  }, []);
-  return (
-    feed && (
-      <>
-        <UserCard user={feed[0]} />
-      </>
-    )
+  }, [pageNo]);
+
+  useEffect(() => {
+    if (feed && feed.length <= 1 && hasMore) {
+      setPageNo((prev) => prev + 1);
+    }
+  }, [feed]);
+  if (!hasMore && feed.length == 0) {
+    return <h1>No User Found</h1>;
+  }
+  return feed && feed.length > 0 ? (
+    <>
+      <UserCard user={feed[0]} />
+    </>
+  ) : (
+    <div className="flex justify-center">
+      <span className="loading loading-spinner loading-xl "></span>
+    </div>
   );
 };
 export default Feed;
