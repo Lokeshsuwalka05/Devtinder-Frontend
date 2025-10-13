2a766c0426e5b2f53ac3b5b959a4974150727aeb
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-13 16:44:45 +0530

    Modified User Card

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,20 @@
+import axios from "axios";
+import { BASE_URL } from "../utills/constants";
+import { useDispatch } from "react-redux";
+import { removeFeed } from "../features/feed/feedSlice";
+
 const UserCard = ({ user }) => {
-  const { firstName, lastName, photoUrl, age, gender, about, skills } = user;
+  const { firstName, lastName, photoUrl, age, gender, about, skills, _id } =
+    user;
+  const dispatch = useDispatch();
+  const handleClick = async (status, userId) => {
+    await axios.post(
+      `${BASE_URL}/request/send/${status}/${userId}`,
+      {},
+      { withCredentials: true }
+    );
+    dispatch(removeFeed(userId));
+  };
   return (
     user && (
       <div className="card bg-base-300 w-72 shadow-sm m-auto mt-5 mb-28">
@@ -18,7 +33,12 @@ const UserCard = ({ user }) => {
           <p className="font-semibold text-gray-600">{skills}</p>
           <p className="font-semibold text-gray-600">{about}</p>
           <div className="card-actions justify-between">
-            <button className="btn btn-secondary">Interested</button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => handleClick("interested", _id)}
+            >
+              Interested
+            </button>
             <button className="btn btn-primary">Ignored</button>
           </div>
         </div>
