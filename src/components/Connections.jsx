ea62dfbfb54ec8ba74d280cf4ec11f5c7e11be66
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-13 16:42:53 +0530

    Bug Fixed

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -22,30 +22,32 @@ const Connections = () => {
   }, []);
 
   return (
-    <ul className="list bg-base-100 rounded-box shadow-md w-1/2 m-auto pb-0 mt-7 mb-24">
-      <li className="p-4 pb-2 text-xs opacity-60 tracking-wide m-auto">
-        Connections
-      </li>
+    <>
+      <ul className="list bg-base-100 rounded-box shadow-md w-1/2 m-auto pb-0 mt-7 mb-24">
+        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide m-auto">
+          Connections
+        </li>
 
-      {connections &&
-        connections.map((connection) => {
-          const { firstName, lastName, age, gender, photoUrl } = connection;
-          return (
-            <li className="list-row">
-              <div>
-                <img className="size-10 rounded-box" src={photoUrl} />
-              </div>
-              <div>
-                <div>{firstName + " " + lastName}</div>
-                <div className="text-xs uppercase font-semibold opacity-60">
-                  {age && <span>{age}</span>}
-                  {gender && <span>{"," + gender}</span>}
+        {connections &&
+          connections.map((connection) => {
+            const { firstName, lastName, age, gender, photoUrl } = connection;
+            return (
+              <li className="list-row">
+                <div>
+                  <img className="size-10 rounded-box" src={photoUrl} />
                 </div>
-              </div>
-            </li>
-          );
-        })}
-    </ul>
+                <div>
+                  <div>{firstName + " " + lastName}</div>
+                  <div className="text-xs uppercase font-semibold opacity-60">
+                    {age && <span>{age}</span>}
+                    {gender && <span>{"," + gender}</span>}
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+      </ul>
+    </>
   );
 };
 export default Connections;
