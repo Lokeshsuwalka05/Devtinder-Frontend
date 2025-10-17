38c323eb99656d0b4e536f42379697debb0f4347
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-17 19:02:26 +0530

    Fixed bug for navigation

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../utills/constants";
 import { useNavigate } from "react-router";
@@ -11,23 +11,29 @@ import { addUser } from "../features/user/userSlice";
 const Body = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const fetchedUser = async () => {
-    try {
-      const userData = await axios.get(BASE_URL + "/profile/view", {
-        withCredentials: true,
-      });
-      dispatch(addUser(userData.data.user));
-    } catch (err) {
-      if (err.status === 401) {
-        navigate("/login");
-      }
-      console.error(err);
-    }
-  };
-  useEffect(() => {
-    fetchedUser();
-  }, []);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchedUser = async () => {
+      try {
+        const userData = await axios.get(BASE_URL + "/profile/view", {
+          withCredentials: true,
+        });
+        dispatch(addUser(userData.data.user));
+      } catch (err) {
+        console.log(err);
+        if (err?.response?.status === 401) {
+          navigate("/login");
+        }
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchedUser();
+  }, [dispatch, navigate]);
+
+  if (loading) return null;
   return (
     <>
       <Navbar />
