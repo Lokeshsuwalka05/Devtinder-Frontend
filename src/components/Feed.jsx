1087a8e438324a815ba0960ca0739e1cc503d3f3
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-23 20:10:34 +0530

    Bug:fixed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,64 +1,62 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "../utills/constants";
 import axios from "axios";
 import UserCard from "./UserCard";
 import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../features/feed/feedSlice";
+import Loader from "./Loader";
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const getFeed = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/feed`, {
+        withCredentials: true,
+      });
+      console.log(res);
+      const users = res?.data?.feedOfUser || [];
+      if (users.length === 0) {
+        setLoading(false);
+      }
+      dispatch(addFeed(users));
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   // Initial load - only when feed is null/empty and we have more data
   useEffect(() => {
-    const getFeed = async () => {
-      try {
-        const res = await axios.get(`${BASE_URL}/feed`, {
-          withCredentials: true,
-        });
-        const users = res?.data?.feedOfUser || [];
-
-        if (users.length === 0) {
-          return;
-        }
-
-        dispatch(addFeed(users));
-      } catch (e) {
-        console.error(e);
-      }
-    };
-
     getFeed();
   }, [dispatch]);
 
+  useEffect(() => {
+    if (feed && feed.length === 0 && loading) {
+      getFeed();
+    }
+  }, [feed, loading]); // watch feed changes
+
   // No more data and feed is empty
   if (!feed) {
-    return (
-      <div className="flex justify-center mt-24">
-        <span className="loading loading-spinner loading-xl"></span>
-      </div>
-    );
+    return;
   }
-  if (feed.length === 0) {
-    return (
-      <h1 className="text-center mt-10 text-xl font-semibold">No User Found</h1>
+  if (feed.length <= 0) {
+    return loading ? (
+      <Loader />
+    ) : (
+      <h1 className="font-semibold text-center text-2xl relative top-28">
+        No User Found
+      </h1>
     );
   }
 
   return (
-    <div className="mb-28">
-      {feed && feed.length > 0 ? (
-        <>
-          <p className="text-center text-gray-800 font-medium mb-6 text-xl">
-            Let's connect with awesome developers
-          </p>
-          <UserCard key={feed[0]._id} user={feed[0]} />
-        </>
-      ) : (
-        <div className="flex justify-center mt-24">
-          <span className="loading loading-spinner loading-xl"></span>
-        </div>
-      )}
+    <div className="mb-32">
+      <p className="text-center text-gray-800 font-medium mb-6 text-xl">
+        Let's connect with awesome developers
+      </p>
+      {feed && <UserCard key={feed[0]._id} user={feed[0]} />}
     </div>
   );
 };
