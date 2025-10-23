3fb2d66c267d22ee49ebc67f617d59397c34eb60
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-23 20:12:08 +0530

    Added margin bottom

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
     }
   };
   return (
-    <div className="card w-96 bg-base-100 card-md shadow-sm m-auto top-5 mb-44">
+    <div className="card w-96 bg-base-100 card-md shadow-sm m-auto top-5 mb-28">
       <div className="card-body">
         <h2 className="card-title">{isLoginForm ? "Login" : "Sign Up"}</h2>
         <fieldset className="fieldset">
