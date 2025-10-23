b1d213a82e7544126e49d88f5b9d8cc03b0aa476
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-23 20:14:18 +0530

    ADD:loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center mt-24">
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+};
+export default Loader;
