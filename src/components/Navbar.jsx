fd459bdda127352968bd8c4108b1962a40bd9dc5
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-10-26 20:07:05 +0530

    ADD:theme toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { BASE_URL } from "../utills/constants";
 import axios from "axios";
 import { removeUser } from "../features/user/userSlice";
 import toast from "react-hot-toast";
+import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
@@ -29,49 +30,53 @@ const Navbar = () => {
           Devtinder
         </Link>
       </div>
-
-      {user && (
-        <div className="flex gap-2 items-center">
-          <p className="font-semibold">
-            Welcome,
-            {user?.firstName
-              ? user.firstName.charAt(0).toUpperCase() +
-                user.firstName.slice(1).toLowerCase()
-              : ""}
-          </p>
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img alt="User Photo" src={user?.photoUrl} />
+      <div className="flex-1 flex justify-center">
+        <ThemeToggle />
+      </div>
+      <div className="flex-1 flex justify-end items-center gap-2">
+        {user && (
+          <div className="flex gap-2 items-center">
+            <p className="font-semibold">
+              Welcome,
+              {user?.firstName
+                ? user.firstName.charAt(0).toUpperCase() +
+                  user.firstName.slice(1).toLowerCase()
+                : ""}
+            </p>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img alt="User Photo" src={user?.photoUrl} />
+                </div>
               </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <Link to="/profile" className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/connections">Connections</Link>
+                </li>
+                <li>
+                  <Link to="/requests">Request Received</Link>
+                </li>
+                <li>
+                  <a onClick={handleLogout}>Logout</a>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <Link to="/profile" className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/connections">Connections</Link>
-              </li>
-              <li>
-                <Link to="/requests">Request Received</Link>
-              </li>
-              <li>
-                <a onClick={handleLogout}>Logout</a>
-              </li>
-            </ul>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
