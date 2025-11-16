aaccdee684b999458493f53addaf70eab52f39af
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-11-16 15:07:18 +0530

    Premium component UI refreshment

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,4 +1,43 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import gold_icon from "../assets/coin.png";
+import silver_icon from "../assets/silver-badge.png";
 const Premium = () => {
-  return <>Hello</>;
+  return (
+    <motion.div
+      className="flex md:w-[90%] w-full m-auto mb-36 min-h-48 md:mb-32 relative top-20 "
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.3 }}
+    >
+      <div className="card bg-base-200 w-[40%] rounded-box grid  grow place-items-center p-5">
+        <div className="flex flex-col md:flex-row  gap-2">
+          <img src={silver_icon} className="size-8 md:size-12"></img>
+          <h1 className="font-bold text-xl md:text-3xl">Silver Membership</h1>
+        </div>
+        <ul className="font-light text-sm mb-2">
+          <li> - Chat with other people</li>
+          <li> - 100 connection Requests per day</li>
+          <li> - Blue Tick</li>
+          <li> - 3 months</li>
+        </ul>
+        <button className="btn btn-dash">Buy Silver</button>
+      </div>
+      <div className="divider divider-horizontal w-[20%]">OR</div>
+      <div className="card bg-base-200 rounded-box w-[40%] grid grow place-items-center p-5">
+        <div className="flex flex-col md:flex-row gap-2">
+          <img src={gold_icon} className="size-5 md:size-8"></img>
+          <h1 className="font-bold text-xl md:text-3xl">Gold Membership</h1>
+        </div>
+        <ul className="font-light text-sm">
+          <li> - Chat with other people</li>
+          <li> - Inifiniye connection Requests per day</li>
+          <li> - Blue Tick</li>
+          <li> - 6 months</li>
+        </ul>
+        <button className="btn btn-dash btn-warning">Buy Gold</button>
+      </div>
+    </motion.div>
+  );
 };
 export default Premium;
